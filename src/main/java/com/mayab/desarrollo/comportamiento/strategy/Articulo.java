8d0f6426d4e45579f3d92e4f8c65c8c68e1fbab6
elrayodev <eloy.jdl@gmail.com>
2021-02-25 14:07:39 -0600

    ULTIMO PUSH

---
+++ src/main/java/com/mayab/desarrollo/comportamiento/strategy/Articulo.java
@@ -0,0 +1,15 @@
+package com.mayab.desarrollo.comportamiento.strategy;
+
+public class Articulo {
+	
+	private String nombre;
+	
+	public Articulo(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+}
